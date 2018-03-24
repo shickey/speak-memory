47d1ed4f63b9b645c60ef9deff162192cbdafff9
Sean Hickey <hisean@media.mit.edu>
2018-03-23 21:30:56 -0400

    Added eslint

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Navbar from './Navbar'
-import DiscussionPlayer from './DiscussionPlayer'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './Navbar';
+import DiscussionPlayer from './DiscussionPlayer';
 
 const App = () => (
   <div>
     <Navbar />
     <DiscussionPlayer />
   </div>
-)
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
