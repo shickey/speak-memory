b8168069f15a888680b032024c0ca60206a2b09e
Sean Hickey <hisean@media.mit.edu>
2018-03-26 19:03:08 -0400

    Discussion data now fully loaded from firebase

--- db/init-db.js
+++ db/init-db.js
@@ -9,8 +9,9 @@
  * 26 Mar 2018
  *
  */
+
  
- var firebase = require('firebase');
+var firebase = require('firebase');
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -25,270 +26,162 @@ firebase.initializeApp(firebaseConfig);
 
 // @NOTE: At this point, firebase storage doesn't work in node environments
 //        so we just upload the asset files manually to firebase storage.
-//
-// var storage = firebase.storage().ref();
-
-const data = [
-  {
-    id: 1,
-    author: 'Gustaaf Gunnhildr',
-    avatar: 'user-0.png',
-    file: 'betterwatchout-1.webm'
-  },
-  {
-    id: 2,
-    author: 'Radha Manisha',
-    avatar: 'user-7.png',
-    file: 'betterwatchout-2.webm'
-  },
-  {
-    id: 3,
-    author: 'Branda Rajesh',
-    avatar: 'user-18.png',
-    file: 'betterwatchout-3.webm'
-  },
-  {
-    id: 4,
-    author: 'Vassiliki Aucaman',
-    avatar: 'user-1.png',
-    file: 'betterwatchout-4.webm'
-  },
-  {
-    id: 5,
-    author: 'Sikke Fflur',
-    avatar: 'user-22.png',
-    file: 'betterwatchout-5.webm'
-  },
-  {
-    id: 6,
-    author: 'Deepti Jonas',
-    avatar: 'user-16.png',
-    file: 'betterwatchout-6.webm'
-  },
-  {
-    id: 7,
-    author: 'Omolara Cecilia',
-    avatar: 'user-11.png',
-    file: 'betterwatchout-7.webm'
-  },
-  {
-    id: 8,
-    author: 'Valentine Eunike',
-    avatar: 'user-19.png',
-    file: 'betterwatchout-8.webm'
-  },
-  {
-    id: 9,
-    author: 'Sigimund Urban',
-    avatar: 'user-2.png',
-    file: 'betterwatchout-9.webm'
-  }
-];
-
-
-// // Avatar images from roundicons
-
-// // @TODO: Move author info out of topics and posts?
-// //        Maybe make authors an object inside each topic/set of posts?
-
-// // @NOTE: The keys used in the topics and posts objects get
-// //        replaced by firebase keys when pushing to the DB.
-// //        But they're still useful in this context since they
-// //        allow us to define keyed relationships without knowing
-// //        the keys beforehand.
-// const topics = {
-//   1: {
-//     title: "Hello world!",
-//     root: 3,
-//   },
-//   2: {
-//     title: "Foo bar baz!",
-//     root: 1,
-//   }
-// }
-
-// const posts = {
-//   1: { // Topic ID
-//     1: { // Post IDs
-//       content: "Praesent ut rhoncus nibh. Vestibulum vitae consectetur diam. Morbi nec porttitor tellus",
-//       author: "Gustaaf Gunnhildr",
-//       avatar: "user-0.png",
-//       parent: 3,
-//       children: {
-//         5: true,
-//         6: true
-//       }
-//     },
-//     2: {
-//       content: "Aliquam tincidunt, metus sed semper accumsan, est dolor maximus dui, ut ullamcorper diam turpis et mi.",
-//       author: "Radha Manisha",
-//       avatar: "user-7.png",
-//       parent: 3,
-//       children: {}
-//     },
-//     3: {
-//       content: "Sed est urna, pretium ac semper quis, dictum elementum ipsum. Integer malesuada condimentum ex vehicula congue. Nunc laoreet tortor lectus, at convallis neque fermentum non. Proin fringilla congue felis, a pulvinar dui luctus ut. Nam sagittis tempus metus, id consectetur dui commodo in. Vestibulum sit amet tortor turpis. Donec lobortis condimentum elit, eu euismod ante. Mauris porttitor in massa at imperdiet. Nam pretium elementum tortor in sagittis.",
-//       author: "Branda Rajesh",
-//       avatar: "user-18.png",
-//       parent: null,
-//       children: {
-//         1 : true,
-//         2 : true,
-//         4 : true,
-//         7 : true,
-//         9 : true
-//       }
-//     },
-//     4: {
-//       content: "Sed vitae auctor ligula, et laoreet nisi.",
-//       author: "Vassiliki Aucaman",
-//       avatar: "user-1.png",
-//       parent: 3,
-//       children: {
-//         8: true
-//       }
-//     },
-//     5: {
-//       content: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus massa metus, molestie eget porta eu, vehicula in libero.",
-//       author: "Sikke Fflur",
-//       avatar: "user-22.png",
-//       parent: 1,
-//       children: {}
-//     },
-//     6: {
-//       content: "Vestibulum fermentum mattis erat. Sed at diam in ex cursus condimentum. Suspendisse potenti. Nullam fringilla lectus tempus libero posuere, quis semper orci semper.",
-//       author: "Deepti Jonas",
-//       avatar: "user-16.png",
-//       parent: 1,
-//       children: {}
-//     },
-//     7: {
-//       content: "Nam bibendum sodales viverra.",
-//       author: "Omolara Cecilia",
-//       avatar: "user-11.png",
-//       parent: 3,
-//       children: {}
-//     },
-//     8: {
-//       content: "Nunc sapien ipsum, cursus sit amet cursus quis, auctor vitae erat. Vivamus fermentum ut lectus iaculis vehicula.",
-//       author: "Valentine Eunike",
-//       avatar: "user-19.png",
-//       parent: 4,
-//       children: {}
-//     },
-//     9: {
-//       content: "Phasellus tellus augue, malesuada eu erat quis, iaculis interdum nibh. Curabitur eros nunc, rutrum quis dolor vel, sodales rhoncus magna."
-//       ,
-//       author: "Sigimund Urban",
-//       avatar: "user-2.png",
-//       parent: 3,
-//       children: {}
-//     }
-//   },
-//   2: { // Topic ID
-//     1: { // Post IDs
-//       content: "Nam varius, purus et malesuada dapibus, arcu ipsum sollicitudin lectus, vitae tempor enim dui eget quam. Sed nisi nisl, fermentum id lobortis vitae, consectetur sed orci. Pellentesque congue ligula nibh, id hendrerit orci iaculis non. Vivamus commodo mattis iaculis.",
-//       author: "Eldon Hildigardis",
-//       avatar: "user-22.png",
-//       parent: null,
-//       children: {
-//         2: true
-//       }
-//     },
-//     2: {
-//       content: "Fusce maximus venenatis magna vitae tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
-//       author: "Rosa Chan",
-//       avatar: "user-3.png",
-//       parent: 1,
-//       children: {
-//         3: true
-//       }
-//     },
-//     3: {
-//       content: "Sed ullamcorper quis augue nec euismod. Integer consectetur dolor elit, varius commodo est cursus vitae. Phasellus rutrum dignissim sem, et euismod est ornare nec. Sed nec arcu sem. Nunc tristique elementum sapien, ac commodo ex vestibulum sed. Mauris eu massa non lorem rutrum sagittis.",
-//       author: "Ben Rhoda",
-//       avatar: "user-9.png",
-//       parent: 2,
-//       children: {
-//         4: true
-//       }
-//     },
-//     4: {
-//       content: "Mauris ut aliquet leo. Quisque vitae tincidunt lorem, id tincidunt sapien.",
-//       author: "Teuta Bityah",
-//       avatar: "user-11.png",
-//       parent: 3,
-//       children: {}
-//     },
-//   }
-// }
-
-
-// var db = firebase.database();
-
-// // Reset db
-// db.ref('/').set({
-//   topics: {},
-//   posts: {}
-// }).then(() => {
-
-//   // Reinitialize topics and replies
-//   var topicsRef = db.ref('topics');
-//   var postsRef = db.ref('posts');
-
-//   var topicsUpdates = {};
-//   var postsUpdates = {};
-
-//   Object.keys(topics).forEach( (topicId, idx) => {
-//     var topicKey = topicsRef.push().key;
-    
-
-//     var topicPosts = posts[topicId];
-//     var idMapping = {}; // Key is the id for the post in the structure
-//                         // written at the top of this file, value is the
-//                         // firebase key it will live at when pushed
-
-//     var rootPost = null;
-//     Object.keys(topicPosts).forEach( (postId, idx) => {
-//       idMapping[postId] = postsRef.push().key;
-//     })
-
-//     Object.keys(topicPosts).forEach( (postId, idx) => {
-//       var post = Object.assign({}, topicPosts[postId]); // Create a copy, just for good measure
-//       if (post.parent) {
-//         post.parent = idMapping[post.parent];
-//       }
-//       else {
-//         rootPost = post;
-//       }
-//       if (post.children) {
-//         var newChildren = {};
-//         for (childId in post.children) {
-//           newChildren[idMapping[childId]] = true;
-//         }
-//         post.children = newChildren;
-//       }
-//       postsUpdates[topicKey + '/' + idMapping[postId]] = post;
-//     })
-
-//     var newTopic = Object.assign({}, topics[topicId]);
-//     newTopic.root = idMapping[newTopic.root];
-//     newTopic.content = rootPost.content;
-//     newTopic.author = rootPost.author;
-//     newTopic.avatar = rootPost.avatar;
-//     topicsUpdates[topicKey] = newTopic;
-
-//   })
-
-//   topicsRef.update(topicsUpdates).then(() => {
-//     postsRef.update(postsUpdates).then(() => {
-//       process.exit();
-//     });
-//   });
-
-// });
+//        
+//        Here's the code I used for extracting the persistent URLs out of
+//        the storage bucket which I had to run inside the react app:
+    /******************************************************
+    var avatarUrls = {};
+    var total = 0;
+    var ref = firebase.storage().ref();
+    for (var idx = 0; idx < 24; ++idx) {
+      (() => {
+        var capturedIdx = idx;
+        ref.child(`avatars/user-${capturedIdx}.png`).getDownloadURL().then( (url) => {
+          avatarUrls[`user-${capturedIdx}.png`] = url;
+          total = total + 1;
+          if (total == 24) {
+            console.log(JSON.stringify(avatarUrls, null, 2));
+          }
+        })
+      })();
+    }
+    *******************************************************/
 
 
 
+// Avatar images from roundicons
 
- 
+const avatarUrls = {
+  "user-0.png" : "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-0.png?alt=media&token=0fbd28e7-5641-4bc7-be4f-62471f2be63e",
+  "user-1.png" : "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-1.png?alt=media&token=bc662af1-a548-491d-9597-3f0cea6e90df",
+  "user-2.png" : "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-2.png?alt=media&token=81d4e438-5b14-402d-829f-e5e3124f6a13",
+  "user-3.png" : "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-3.png?alt=media&token=7c3dccea-0d07-497f-b675-97ba82e8d8fe",
+  "user-4.png" : "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-4.png?alt=media&token=7550f71c-0f0e-40a0-b3b3-640c7b0a6643",
+  "user-5.png" : "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-5.png?alt=media&token=8ddbb64b-2b02-401c-aa42-3e48dc5c4bff",
+  "user-6.png" : "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-6.png?alt=media&token=1da424a7-a28c-432d-bcff-3059406b0918",
+  "user-7.png" : "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-7.png?alt=media&token=8ba5a195-527e-4dd1-ad87-c0ecadb0191b",
+  "user-8.png" : "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-8.png?alt=media&token=081aafa6-5fd6-409e-b841-ceb16c25ca93",
+  "user-9.png" : "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-9.png?alt=media&token=73734ab8-bc34-4f13-aab9-0c6e48426f8b",
+  "user-10.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-10.png?alt=media&token=f2ac4249-18b3-493f-aaed-b4ab81db9df5",
+  "user-11.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-11.png?alt=media&token=53820c15-c450-48a2-8ff5-637af10d465d",
+  "user-12.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-12.png?alt=media&token=46bfacfe-220c-4bc3-9456-78e847ac73aa",
+  "user-13.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-13.png?alt=media&token=462e932d-40b9-41a6-920b-ec4a5ba31c5b",
+  "user-14.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-14.png?alt=media&token=5f33172b-5277-4519-b6a3-618f51647883",
+  "user-15.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-15.png?alt=media&token=a4a3d002-63fd-4da2-8d13-ff97ac98086c",
+  "user-16.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-16.png?alt=media&token=00b6b6e9-3680-4b62-8b33-c19a91fa9347",
+  "user-17.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-17.png?alt=media&token=f8f9b246-abf9-4e77-aba7-a31780e04f33",
+  "user-18.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-18.png?alt=media&token=14e3aae6-823a-4a1e-8df6-1df168b4d6ad",
+  "user-19.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-19.png?alt=media&token=0297c63a-7806-4048-8248-77b212b79b80",
+  "user-20.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-20.png?alt=media&token=ba8e148c-da7e-4280-9965-b2b262ca5430",
+  "user-21.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-21.png?alt=media&token=cf9c3fb9-1c98-44d2-ba77-4c0ffa149962",
+  "user-22.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-22.png?alt=media&token=b62f66a4-0587-4c9d-986d-f085b276fb1b",
+  "user-23.png": "https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-23.png?alt=media&token=62ae5673-eabf-45a5-9d47-e57ac9708662"
+}
+
+const videoUrls = {
+  "betterwatchout-1.webm": 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-1.webm?alt=media&token=4647daa5-8443-46ae-8339-9198d55fe3bc',
+  "betterwatchout-2.webm": 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-2.webm?alt=media&token=295b87c4-ff96-4685-983d-22a880c9a7d6',
+  "betterwatchout-3.webm": 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-3.webm?alt=media&token=4d758ed9-a4ac-49da-8bcd-26a1e6b1abd2',
+  "betterwatchout-4.webm": 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-4.webm?alt=media&token=b977fa29-760c-45bd-ab0f-874c300b711d',
+  "betterwatchout-5.webm": 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-5.webm?alt=media&token=5b17d816-3e4e-4f46-a002-f01a38311751',
+  "betterwatchout-6.webm": 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-6.webm?alt=media&token=13ece841-3ed7-4e00-9a40-963b8e78ec9c',
+  "betterwatchout-7.webm": 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-7.webm?alt=media&token=6751ef45-0ff2-4d9d-8a54-ea897333dd47',
+  "betterwatchout-8.webm": 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-8.webm?alt=media&token=9f6bac19-8535-419a-9032-d5300898cec8',
+  "betterwatchout-9.webm": 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-9.webm?alt=media&token=2df98d52-0ae0-42a1-ae9f-46e4a49b720a'
+}
+
+// We insert these records into firebase in order, so that their keys will be timestamped in the order we want
+const discussions = {
+  1: [
+    {
+      author: 'Gustaaf Gunnhildr',
+      avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-0.png?alt=media&token=0fbd28e7-5641-4bc7-be4f-62471f2be63e',
+      videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-1.webm?alt=media&token=4647daa5-8443-46ae-8339-9198d55fe3bc'
+    },
+    {
+      author: 'Radha Manisha',
+      avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-7.png?alt=media&token=8ba5a195-527e-4dd1-ad87-c0ecadb0191b',
+      videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-2.webm?alt=media&token=295b87c4-ff96-4685-983d-22a880c9a7d6'
+    },
+    {
+      author: 'Branda Rajesh',
+      avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-18.png?alt=media&token=14e3aae6-823a-4a1e-8df6-1df168b4d6',
+      videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-3.webm?alt=media&token=4d758ed9-a4ac-49da-8bcd-26a1e6b1abd2'
+    },
+    {
+      author: 'Vassiliki Aucaman',
+      avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-1.png?alt=media&token=bc662af1-a548-491d-9597-3f0cea6e90df',
+      videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-4.webm?alt=media&token=b977fa29-760c-45bd-ab0f-874c300b711d'
+    },
+    {
+      author: 'Sikke Fflur',
+      avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-22.png?alt=media&token=b62f66a4-0587-4c9d-986d-f085b276fb1b',
+      videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-5.webm?alt=media&token=5b17d816-3e4e-4f46-a002-f01a38311751'
+    },
+    {
+      author: 'Deepti Jonas',
+      avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-16.png?alt=media&token=00b6b6e9-3680-4b62-8b33-c19a91fa9347',
+      videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-6.webm?alt=media&token=13ece841-3ed7-4e00-9a40-963b8e78ec9c'
+    },
+    {
+      author: 'Omolara Cecilia',
+      avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-11.png?alt=media&token=53820c15-c450-48a2-8ff5-637af10d465d',
+      videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-7.webm?alt=media&token=6751ef45-0ff2-4d9d-8a54-ea897333dd47'
+    },
+    {
+      author: 'Valentine Eunike',
+      avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-19.png?alt=media&token=0297c63a-7806-4048-8248-77b212b79b80',
+      videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-8.webm?alt=media&token=9f6bac19-8535-419a-9032-d5300898cec8'
+    },
+    {
+      author: 'Sigimund Urban',
+      avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-2.png?alt=media&token=81d4e438-5b14-402d-829f-e5e3124f6a13',
+      videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-9.webm?alt=media&token=2df98d52-0ae0-42a1-ae9f-46e4a49b720a'
+    }
+  ]
+};
+
+
+
+// Push to firebase
+
+var db = firebase.database();
+
+// Reset db
+db.ref('/').set({
+  discussions: {},
+}).then(() => {
+
+  // Reinitialize discussions
+  var discussionsRef = db.ref('discussions');
+
+  var discussionsUpdates = {};
+
+  Object.keys(discussions).forEach( (discussionId, idx) => {
+    var discussionKey = discussionsRef.push().key;
+
+    var entriesRef = db.ref(`discussions/${discussionKey}`)
+
+    var discussionEntries = discussions[discussionId];
+    var discussionEntryUpdates = {};
+
+    discussionEntries.forEach( (entry, idx) => {
+      var entryKey = entriesRef.push().key;
+      var entryUpdate = discussionEntries[idx];
+      entryUpdate.id = entryKey;
+      discussionEntryUpdates[entryKey] = entryUpdate;
+    })
+
+    discussionsUpdates[discussionKey] = discussionEntryUpdates;
+
+    console.log(discussionsUpdates);
+
+  });
+
+  discussionsRef.update(discussionsUpdates).then(() => {
+    process.exit();
+  });
+
+});
+
+
--- src/js/DiscussionPlayer.jsx
+++ src/js/DiscussionPlayer.jsx
@@ -1,64 +1,10 @@
 import React from 'react';
-import VideoPlayer from './VideoPlayer';
-
+import { connect } from 'react-redux'
+import { compose } from 'redux'
 import firebase from 'firebase';
+import { firebaseConnect, getVal } from 'react-redux-firebase'
 
-const data = [
-  {
-    id: 1,
-    author: 'Gustaaf Gunnhildr',
-    avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-0.png?alt=media&token=0fbd28e7-5641-4bc7-be4f-62471f2be63e',
-    videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-1.webm?alt=media&token=4647daa5-8443-46ae-8339-9198d55fe3bc'
-  },
-  {
-    id: 2,
-    author: 'Radha Manisha',
-    avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-7.png?alt=media&token=8ba5a195-527e-4dd1-ad87-c0ecadb0191b',
-    videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-2.webm?alt=media&token=295b87c4-ff96-4685-983d-22a880c9a7d6'
-  },
-  {
-    id: 3,
-    author: 'Branda Rajesh',
-    avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-18.png?alt=media&token=14e3aae6-823a-4a1e-8df6-1df168b4d6',
-    videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-3.webm?alt=media&token=4d758ed9-a4ac-49da-8bcd-26a1e6b1abd2'
-  },
-  {
-    id: 4,
-    author: 'Vassiliki Aucaman',
-    avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-1.png?alt=media&token=bc662af1-a548-491d-9597-3f0cea6e90df',
-    videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-4.webm?alt=media&token=b977fa29-760c-45bd-ab0f-874c300b711d'
-  },
-  {
-    id: 5,
-    author: 'Sikke Fflur',
-    avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-22.png?alt=media&token=b62f66a4-0587-4c9d-986d-f085b276fb1b',
-    videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-5.webm?alt=media&token=5b17d816-3e4e-4f46-a002-f01a38311751'
-  },
-  {
-    id: 6,
-    author: 'Deepti Jonas',
-    avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-16.png?alt=media&token=00b6b6e9-3680-4b62-8b33-c19a91fa9347',
-    videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-6.webm?alt=media&token=13ece841-3ed7-4e00-9a40-963b8e78ec9c'
-  },
-  {
-    id: 7,
-    author: 'Omolara Cecilia',
-    avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-11.png?alt=media&token=53820c15-c450-48a2-8ff5-637af10d465d',
-    videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-7.webm?alt=media&token=6751ef45-0ff2-4d9d-8a54-ea897333dd47'
-  },
-  {
-    id: 8,
-    author: 'Valentine Eunike',
-    avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-19.png?alt=media&token=0297c63a-7806-4048-8248-77b212b79b80',
-    videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-8.webm?alt=media&token=9f6bac19-8535-419a-9032-d5300898cec8'
-  },
-  {
-    id: 9,
-    author: 'Sigimund Urban',
-    avatarUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/avatars%2Fuser-2.png?alt=media&token=81d4e438-5b14-402d-829f-e5e3124f6a13',
-    videoUrl: 'https://firebasestorage.googleapis.com/v0/b/speak-memory.appspot.com/o/videos%2Fbetterwatchout-9.webm?alt=media&token=2df98d52-0ae0-42a1-ae9f-46e4a49b720a'
-  }
-];
+import VideoPlayer from './VideoPlayer';
 
 class DiscussionPlayer extends React.Component {
 
@@ -69,15 +15,6 @@ class DiscussionPlayer extends React.Component {
     };
     this.avatarClicked = this.avatarClicked.bind(this);
     this.onVideoEnd = this.onVideoEnd.bind(this);
-
-    // var ref = firebase.storage().ref();
-    // data.forEach( d => {
-    //   ref.child('videos/' + d.file).getDownloadURL().then( (url) => {
-    //     console.log(d.id);
-    //     console.log(url);
-    //   })
-    // })
-
   }
 
   avatarClicked(idx) {
@@ -86,7 +23,7 @@ class DiscussionPlayer extends React.Component {
   }
 
   onVideoEnd() {
-    if (this.state.currentVideoIdx < data.length - 1) {
+    if (this.state.currentVideoIdx < this.props.data.length - 1) {
       this.setState({currentVideoIdx: this.state.currentVideoIdx + 1});
     }
     else {
@@ -96,8 +33,11 @@ class DiscussionPlayer extends React.Component {
 
   render() {
 
+    if (!this.props.data) { return null; }
+
     var avatars = [];
-    data.forEach( (d, idx) => {
+
+    this.props.data.forEach( (d, idx) => {
       var pullClass = '';
       var currentVideoIdx = this.state.currentVideoIdx;
       if (currentVideoIdx !== null) {
@@ -120,7 +60,7 @@ class DiscussionPlayer extends React.Component {
 
     return (
       <div className="player-container">
-        <VideoPlayer videoInfo={this.state.currentVideoIdx !== null ? data[this.state.currentVideoIdx] : null } onVideoEnd={() => this.onVideoEnd()} />
+        <VideoPlayer videoInfo={this.state.currentVideoIdx !== null ? this.props.data[this.state.currentVideoIdx] : null } onVideoEnd={() => this.onVideoEnd()} />
         <div className="avatars-container">
           <div className={ this.state.currentVideoIdx !== null ? 'avatars-line playing' : 'avatars-line'} ></div>
           {avatars}
@@ -131,4 +71,25 @@ class DiscussionPlayer extends React.Component {
 
 }
 
-export default DiscussionPlayer;
+const mapStateToProps = (state, props) => {
+  return {
+    data: (() => {
+      // @TODO: Is this reasonable? Or ridiculous?
+      //        Most importantly: does it guarantee ordering by timestamp?
+      var fbData = getVal(state.firebase, `data/discussions/-L8ZSju-G6FzkaaCOe39`);
+      if (fbData) {
+        return Object.values(fbData);
+      }
+      return undefined;
+    })()
+  }
+};
+
+export default compose(
+  firebaseConnect((props) => {
+    return [
+      `discussions/-L8ZSju-G6FzkaaCOe39`
+    ]
+  }),
+  connect(mapStateToProps)
+)(DiscussionPlayer);
