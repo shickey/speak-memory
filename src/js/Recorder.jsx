18d85408cb87ddd9155adf21d6bbb74d4c1956d1
Sean Hickey <hisean@media.mit.edu>
2018-04-04 19:49:02 -0400

    Implemented basic recording flow

--- src/js/Recorder.jsx
+++ src/js/Recorder.jsx
@@ -3,10 +3,16 @@ import RecordRTC from 'recordrtc';
 
 const hasGetUserMedia = !!(navigator.mediaDevices.getUserMedia);
 
-const cameraAccessibility = {
-  UNKNOWN: "UNKNOWN",
-  NO: "NO",
-  YES: "YES"
+const RecordingStates = {
+  WAITING: "WAITING",
+  NO_CAMERA: "NO_CAMERA",
+  READY: "READY",
+  RECORDING: "RECORDING",
+  PLAYBACK_READY: "PLAYBACK_READY",
+  PLAYBACK_PLAY: "PLAYBACK_PLAY",
+  UPLOADING: "UPLOADING",
+  UPLOAD_SUCCESS: "UPLOAD_SUCCESS",
+  UPLOAD_FAILURE: "UPLOAD_FAILURE"
 }
 
 class Recorder extends React.Component {
@@ -15,17 +21,28 @@ class Recorder extends React.Component {
     super(props);
 
     this.state = {
-      cameraAccessible: cameraAccessibility.UNKNOWN,
+      recordingState: RecordingStates.WAITING,
       stream: null,
       src: null,
       recordingVideo: null
     }
 
+    this.videoPlaybackReachedEnd = this.videoPlaybackReachedEnd.bind(this);
     this.startRecording = this.startRecording.bind(this);
     this.stopRecording = this.stopRecording.bind(this);
+    this.startPlayback = this.startPlayback.bind(this);
+    this.stopPlayback = this.stopPlayback.bind(this);
+    this.cancelPlayback = this.cancelPlayback.bind(this);
+    this.beginUpload = this.beginUpload.bind(this);
+    this.uploadSuccessOkay = this.uploadSuccessOkay.bind(this);
+    this.uploadFailureCancel = this.uploadFailureCancel.bind(this);
+    this.uploadFailureRetry = this.uploadFailureRetry.bind(this);
   }
 
   componentDidMount() {
+    this.setState({
+      recordingState: RecordingStates.WAITING
+    })
     // Begin camera
     navigator.mediaDevices.getUserMedia({
       audio: true,
@@ -34,84 +51,279 @@ class Recorder extends React.Component {
         height: 450
       }
     }).then( (stream) => {
-      console.log("then");
       this.setState({ 
-        cameraAccessible: cameraAccessibility.YES,
+        recordingState: RecordingStates.READY,
         stream: stream,
-        src: window.URL.createObjectURL(stream)
+        src: window.URL.createObjectURL(stream) // TODO: Fix warning
       });
     }).catch( (err) => {
       this.setState({
-        cameraAccessible: cameraAccessibility.NO,
+        recordingState: RecordingStates.NO_CAMERA,
         stream: null,
         src: null
       })
     })
   }
 
+  componentWillUnmount() {
+    // Stop the camera, if it's on
+    if (this.state.stream) {
+      this.state.stream.getTracks().forEach( t => t.stop() );
+    }
+    this.setState({
+      recordingState: RecordingStates.WAITING,
+      stream: null,
+      src: null,
+      recordingVideo: null
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.recordingState === RecordingStates.READY) {
+      this.videoElement.play();
+    }
+    else if (this.state.recordingState === RecordingStates.PLAYBACK_READY) {
+      this.videoElement.pause();
+    }
+    else if (this.state.recordingState === RecordingStates.PLAYBACK_PLAY) {
+      this.videoElement.play();
+      this.videoElement.addEventListener('ended', this.videoPlaybackReachedEnd);
+    }
+  }
+
+  videoPlaybackReachedEnd() {
+    this.videoElement.currentTime = 0;
+    this.videoElement.removeEventListener('ended', this.videoPlaybackReachedEnd);
+    this.setState({
+      recordingState: RecordingStates.PLAYBACK_READY
+    });
+  }
+
   startRecording() {
-    console.log("start");
     var record = RecordRTC(this.state.stream, { 
       mimeType: 'video/webm\;codecs=vp9',
       bitsPerSecond: 256000
     });
     record.startRecording();
     this.setState({
+      recordingState: RecordingStates.RECORDING,
       recordingVideo: record
     })
   }
 
   stopRecording() {
-    console.log("stop");
     if (this.state.recordingVideo) {
       this.state.recordingVideo.stopRecording( (blobUrl) => {
         
-        var blob = this.state.recordingVideo.blob;
-        console.log(blob);
-
-        // Do stuff with the blobbbbbb
-
-
-        // // Download
-        // var a = document.createElement("a");
-        // document.body.appendChild(a);
-        // a.style = "display: none";
-        // a.href = blobUrl;
-        // a.download = "foobar.webm";
-        // a.click();
-
         this.setState({
-          recordingVideo: null
+          recordingState: RecordingStates.PLAYBACK_READY,
+          recordingVideo: null,
+          src: blobUrl
         });
+
       });
+    }
+  }
+
+  startPlayback() {
+    this.setState({
+      recordingState: RecordingStates.PLAYBACK_PLAY
+    });
+  }
+
+  stopPlayback() {
+    this.videoElement.currentTime = 0; // Go back to the beginning
+    this.setState({
+      recordingState: RecordingStates.PLAYBACK_READY
+    });
+  }
+
+  cancelPlayback() {
+    this.setState({
+      recordingState: RecordingStates.READY,
+      src: window.URL.createObjectURL(this.state.stream)
+    });
+  }
+
+  beginUpload() {
+    this.setState({
+      recordingState: RecordingStates.UPLOADING
+    });
+    setTimeout( () => {
+      this.setState({
+        recordingState: RecordingStates.UPLOAD_SUCCESS
+      });
+    }, 5000)
+  }
+
+  uploadSuccessOkay() {
+    this.setState({
+      recordingState: RecordingStates.READY,
+      src: window.URL.createObjectURL(this.state.stream)
+    });
+  }
+
+  uploadFailureCancel() {
+    this.setState({
+      recordingState: RecordingStates.READY,
+      src: window.URL.createObjectURL(this.state.stream)
+    });
+  }
+
+  uploadFailureRetry() {
+    this.beginUpload();
+  }
+
+  // TODO: Remove eventually
+  debugStringForRecordingState(recordingState) {
+    switch (recordingState) {
+      case RecordingStates.WAITING:
+        return "WAITING";
+      break;
+      case RecordingStates.NO_CAMERA:
+        return "NO_CAMERA";
+      break;
+      case RecordingStates.READY:
+        return "READY";
+      break;
+      case RecordingStates.RECORDING:
+        return "RECORDING";
+      break;
+      case RecordingStates.PLAYBACK_READY:
+        return "PLAYBACK_READY";
+      break;
+      case RecordingStates.PLAYBACK_PLAY:
+        return "PLAYBACK_PLAY";
+      break;
+      case RecordingStates.UPLOADING:
+        return "UPLOADING";
+      break;
+      case RecordingStates.UPLOAD_SUCCESS:
+        return "UPLOAD_SUCCESS";
+      break;
+      case RecordingStates.UPLOAD_FAILURE:
+        return "UPLOAD_FAILURE";
+      break;
     }
   }
 
   render() {
 
-    var videoOrError = (<video autoPlay className="recorder-video" src={this.state.src}></video>);
-    if (this.state.cameraAccessible === cameraAccessibility.NO) {
-      videoOrError = (<div className="recorder-error">No camera available</div>)
-    }
+    var videoOrInfo = (<video ref={ (element) => this.videoElement = element } className="recorder-video" src={this.state.src}></video>);
+    var recordingControls = (<div className="recorder-controls"></div>);
 
-    var recordButton = (
-      <button className="btn btn-md recorder-record-button" onClick={this.startRecording} disabled={this.state.cameraAccessible !== cameraAccessibility.YES}>
-        <img src="img/record.svg" />Record
-      </button>);
-    if (this.state.recordingVideo) {
-      recordButton = (
-      <button className="btn btn-md recorder-record-button" onClick={this.stopRecording} disabled={this.state.cameraAccessible !== cameraAccessibility.YES}>
-        Stop
-      </button>)
+    switch (this.state.recordingState) {
+
+      case RecordingStates.WAITING:
+        recordingControls = (
+          <div className="recorder-controls">
+            <button className="btn btn-md recorder-controls-button" disabled>
+              <img src="img/record.svg" />Record
+            </button>
+          </div>
+        );
+      break;
+
+      case RecordingStates.NO_CAMERA:
+        videoOrInfo = (<div className="recorder-info">No camera available</div>);
+        recordingControls = (
+          <div className="recorder-controls">
+            <button className="btn btn-md recorder-controls-button" disabled>
+              <img src="img/record.svg" />Record
+            </button>
+          </div>
+        );
+      break;
+
+      case RecordingStates.READY:
+        recordingControls = (
+          <div className="recorder-controls">
+            <button className="btn btn-md recorder-controls-button" onClick={this.startRecording}>
+              <img src="img/record.svg" />Record
+            </button>
+          </div>
+        );
+      break;
+
+      case RecordingStates.RECORDING:
+        recordingControls = (
+          <div className="recorder-controls">
+            <button className="btn btn-md recorder-controls-button" onClick={this.stopRecording}>
+              Stop
+            </button>
+          </div>
+        );
+      break;
+
+      case RecordingStates.PLAYBACK_READY:
+        recordingControls = (
+          <div className="recorder-controls">
+            <button className="btn btn-md recorder-controls-button btn-cancel" onClick={this.cancelPlayback}>
+              Cancel
+            </button>
+            <button className="btn btn-md recorder-controls-button" onClick={this.startPlayback}>
+              Play
+            </button>
+            <button className="btn btn-md recorder-controls-button btn-success" onClick={this.beginUpload}>
+              Save
+            </button>
+          </div>
+        );
+      break;
+
+      case RecordingStates.PLAYBACK_PLAY:
+        recordingControls = (
+          <div className="recorder-controls">
+            <button className="btn btn-md recorder-controls-button btn-cancel" disabled>
+              Cancel
+            </button>
+            <button className="btn btn-md recorder-controls-button" onClick={this.stopPlayback}>
+              Stop
+            </button>
+            <button className="btn btn-md recorder-controls-button btn-success" disabled>
+              Save
+            </button>
+          </div>
+        );
+      break;
+
+      case RecordingStates.UPLOADING:
+        videoOrInfo = (<div className="recorder-info"><div className="pulse">Uploading...</div></div>);
+      break;
+
+      case RecordingStates.UPLOAD_SUCCESS:
+        videoOrInfo = (<div className="recorder-info">Uploaded!</div>);
+        recordingControls = (
+          <div className="recorder-controls" onClick={this.uploadSuccessOkay}>
+            <button className="btn btn-md recorder-controls-button btn-success">
+              Okay
+            </button>
+          </div>
+        );
+      break;
+
+      case RecordingStates.UPLOAD_FAILURE:
+        videoOrInfo = (<div className="recorder-info">Upload failed.</div>);
+        recordingControls = (
+          <div className="recorder-controls">
+            <button className="btn btn-md recorder-controls-button btn-cancel" onClick={this.uploadFailureCancel}>
+              Cancel
+            </button>
+            <button className="btn btn-md recorder-controls-button" onClick={this.uploadFailureRetry}>
+              Retry
+            </button>
+          </div>
+        );
+      break;
+
     }
 
     return (
       <div className="recorder">
+        <div className="debug">{this.debugStringForRecordingState(this.state.recordingState)}</div>
         <h2>Record Your Message</h2>
-        {videoOrError}
-        <div className="recorder-controls">
-          {recordButton}
-        </div>
+        {videoOrInfo}
+        {recordingControls}
       </div>
     );
   }
